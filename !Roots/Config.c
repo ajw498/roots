424851142c805d26ef20c26a9074b86cb00a7aa4
Alex Waugh <alex@alexwaugh.com>
1999-10-12 14:11:39 +0000

    Added Config_GraphicsStyle, Config_Snap, Config_SnapDistance, Config_ScrollSpeed

--- !Roots/Config.c
+++ !Roots/Config.c
@@ -3,13 +3,38 @@
 	© Alex Waugh 1999
 
 	$Log: Config.c,v $
+	Revision 1.2  1999/10/12 14:11:39  AJW
+	Added Config_GraphicsStyle, Config_Snap, Config_SnapDistance, Config_ScrollSpeed
+
 	Revision 1.1  1999/09/27 15:32:22  AJW
 	Initial revision
 
 
 */
 
-void voidfunc(void)
+#include "Desk.Core.h"
+
+static char graphicsstyle[256]="Default";
+static Desk_bool snap=Desk_TRUE;
+static int snapdistance=30;
+static int scrollspeed=1;
+
+char *Config_GraphicsStyle(void)
 {
-	/*Prevent compiler warnings*/
+	return graphicsstyle;
+}
+
+Desk_bool Config_Snap(void)
+{
+	return snap;
+}
+
+int Config_SnapDistance(void)
+{
+	return snapdistance;
+}
+
+int Config_ScrollSpeed(void)
+{
+	return scrollspeed;
 }
