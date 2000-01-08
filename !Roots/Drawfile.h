5f72dcca7b93c55babc2cb08dd1d856ebc6355a9
Alex Waugh <alex@alexwaugh.com>
2000-01-08 16:14:57 +0000

    Initial revision

---
+++ !Roots/Drawfile.h
@@ -0,0 +1,8 @@
+
+void Drawfile_PlotLine(int minx,int miny,int maxx,int maxy,int linethickness,unsigned int colour);
+
+void Drawfile_PlotRectangle(int x,int y,int width,int height,int linethickness,unsigned int colour);
+
+void Drawfile_PlotRectangleFilled(int x,int y,int width,int height,int linethickness,unsigned int colour);
+
+void Drawfile_Save(char *filename,layout *layout);
