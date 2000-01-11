10920086dd0bcdd68e1c2364f86bf657915b79e4
Alex Waugh <alex@alexwaugh.com>
2000-01-11 17:10:49 +0000

    Initial revision

---
+++ !Roots/File.c
@@ -0,0 +1,70 @@
+/*
+	FT - File loading and saving
+	© Alex Waugh 1999
+
+	$Log: File.c,v $
+	Revision 1.1  2000/01/11 17:10:49  AJW
+	Initial revision
+
+
+*/
+
+/*	Includes  */
+
+#include "Desk.Window.h"
+#include "Desk.Error.h"
+#include "Desk.Event.h"
+#include "Desk.EventMsg.h"
+#include "Desk.Handler.h"
+#include "Desk.Hourglass.h"
+#include "Desk.Icon.h"
+#include "Desk.Menu.h"
+#include "Desk.Msgs.h"
+#include "Desk.Drag.h"
+#include "Desk.Resource.h"
+#include "Desk.Screen.h"
+#include "Desk.Template.h"
+#include "Desk.File.h"
+#include "Desk.Filing.h"
+#include "Desk.Sprite.h"
+#include "Desk.GFX.h"
+#include "Desk.Font2.h"
+#include "Desk.ColourTran.h"
+
+#include "AJWLib.Window.h"
+#include "AJWLib.Menu.h"
+#include "AJWLib.Msgs.h"
+#include "AJWLib.Icon.h"
+#include "AJWLib.File.h"
+#include "AJWLib.Flex.h"
+#include "AJWLib.Str.h"
+#include "AJWLib.Draw.h"
+#include "AJWLib.DrawFile.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "Database.h"
+#include "Graphics.h"
+#include "GConfig.h"
+#include "Config.h"
+#include "Layout.h"
+
+#define FILEID "Root"
+#define FILEVERSION 100
+
+
+void File_SaveFile(const char *filename)
+{
+	FILE *file;
+	char fileid[]=FILEID;
+	int fileversion=FILEVERSION;
+	file=AJWLib_File_fopen(filename,"w");
+	AJWLib_File_fwrite(fileid,1,4,file);
+	AJWLib_File_fwrite(&fileversion,4,1,file);
+	Database_Save(file);
+	/*Layouts();*/
+	AJWLib_File_fclose(file);
+}
+
