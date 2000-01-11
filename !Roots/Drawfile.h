d695b5aee82f3fd6677c6a3eaee6c55e0e6f94bd
Alex Waugh <alex@alexwaugh.com>
2000-01-11 13:45:22 +0000

    Removed a const

--- !Roots/Drawfile.h
+++ !Roots/Drawfile.h
@@ -1,12 +1,12 @@
 #ifndef DRAWFILE_H
 #define DRAWFILE_H
 
-void Drawfile_PlotLine(int minx,int miny,int maxx,int maxy,int linethickness,unsigned int colour);
+void Drawfile_PlotLine(const int minx,const int miny,const int maxx,const int maxy,const int linethickness,const unsigned int colour);
 
-void Drawfile_PlotRectangle(int x,int y,int width,int height,int linethickness,unsigned int colour);
+void Drawfile_PlotRectangle(const int x,const int y,const int width,const int height,const int linethickness,const unsigned int colour);
 
-void Drawfile_PlotRectangleFilled(int x,int y,int width,int height,int linethickness,unsigned int colour);
+void Drawfile_PlotRectangleFilled(const int x,const int y,const int width,const int height,const int linethickness,const unsigned int colour);
 
-void Drawfile_Save(char *filename,layout *layout);
+void Drawfile_Save(const char *filename,layout *layout);
 
 #endif
