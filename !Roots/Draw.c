4fc8d283b03a55cb34fe144ab05ec3bc4806674e
Alex Waugh <alex@alexwaugh.com>
2000-01-11 13:32:30 +0000

    Added Draw_SetScaleFactor Added lots of consts

--- !Roots/Draw.c
+++ !Roots/Draw.c
@@ -3,6 +3,10 @@
 	© Alex Waugh 1999
 
 	$Log: Draw.c,v $
+	Revision 1.2  2000/01/11 13:32:30  AJW
+	Added Draw_SetScaleFactor
+	Added lots of consts
+
 	Revision 1.1  2000/01/08 20:00:17  AJW
 	Initial revision
 
@@ -20,29 +24,52 @@
 
 #define SWI_ColourTrans_SetFontColours 0x4074F
 
-extern os_trfm matrix;
+static os_trfm matrix;
 
-void Draw_PlotRectangle(int x,int y,int width,int height,int linethickness,unsigned int colour)
+void Draw_SetScaleFactor(const unsigned int factor)
+{
+	matrix.entries[0][0]=factor;
+	matrix.entries[0][1]=0;
+	matrix.entries[1][0]=0;
+	matrix.entries[1][1]=factor;
+	matrix.entries[2][0]=0;
+	matrix.entries[2][1]=0;
+}
+
+void Draw_PlotRectangle(const int x,const int y,const int width,const int height,const int linethickness,const unsigned int colour)
 {
 	Desk_ColourTrans_SetGCOL(colour,0/*or 1<<8 to use ECFs ?*/,0);
 	AJWLib_Draw_PlotRectangle(x,y,width,height,linethickness,&matrix);
 }
 
-void Draw_PlotRectangleFilled(int x,int y,int width,int height,int linethickness,unsigned int colour)
+void Draw_PlotRectangleFilled(const int x,const int y,const int width,const int height,const int linethickness,const unsigned int colour)
 {
 	Desk_ColourTrans_SetGCOL(colour,0/*or 1<<8 to use ECFs ?*/,0);
 	AJWLib_Draw_PlotRectangleFilled(x,y,width,height,&matrix);
 }
 
-void Draw_PlotLine(int minx,int miny,int maxx,int maxy,int linethickness,unsigned int colour)
+void Draw_PlotLine(const int minx,const int miny,const int maxx,const int maxy,const int linethickness,const unsigned int colour)
 {
 	Desk_ColourTrans_SetGCOL(colour,0/*or 1<<8 to use ECFs ?*/,0);
 	AJWLib_Draw_PlotLine(minx,miny,maxx,maxy,linethickness,&matrix);
 }
 
-void Draw_PlotText(int x,int y,int handle,char *font,int size,unsigned int bgcolour, unsigned int fgcolour,char *text)
+void Draw_PlotText(const int x,const int y,const int handle,const char *font,const int size,const unsigned int bgcolour,const unsigned int fgcolour,const char *text)
 {
 	Desk_Font_SetFont(handle);
 	Desk_SWI(4,0,SWI_ColourTrans_SetFontColours,0,bgcolour,fgcolour,14);
-	Desk_Font_Paint(text,Desk_font_plot_OSCOORS,x,y);
+	Desk_Font_Paint(text,Desk_font_plot_OSCOORS,x,y); /*Use transformation matrix*/
 }
+
+void Draw_EORRectangle(const int x,const int y,const int width,const int height,const int linethickness,const unsigned int colour)
+{
+	Desk_ColourTrans_SetGCOL(colour,0,3);
+	AJWLib_Draw_PlotRectangle(x,y,width,height,linethickness,&matrix);
+}
+
+void Draw_EORRectangleFilled(const int x,const int y,const int width,const int height,const unsigned int colour)
+{
+	Desk_ColourTrans_SetGCOL(colour,0,3);
+	AJWLib_Draw_PlotRectangle(x,y,width,height,0,&matrix);
+}
+
--- !Roots/Draw.h
+++ !Roots/Draw.h
@@ -1,12 +1,18 @@
 #ifndef DRAW_H
 #define DRAW_H
 
-void Draw_PlotRectangle(int x,int y,int width,int height,int linethickness,unsigned int colour);
+void Draw_SetScaleFactor(const unsigned int factor);
 
-void Draw_PlotRectangleFilled(int x,int y,int width,int height,int linethickness,unsigned int colour);
+void Draw_PlotRectangle(const int x,const int y,const int width,const int height,const int linethickness,const unsigned int colour);
 
-void Draw_PlotLine(int minx,int miny,int maxx,int maxy,int linethickness,unsigned int colour);
+void Draw_PlotRectangleFilled(const int x,const int y,const int width,const int height,const int linethickness,const unsigned int colour);
 
-void Draw_PlotText(int x,int y,int handle,char *font,int size,unsigned int bgcolour, unsigned int fgcolour,char *text);
+void Draw_PlotLine(const int minx,const int miny,const int maxx,const int maxy,const int linethickness,const unsigned int colour);
+
+void Draw_PlotText(const int x,const int y,const int handle,const char *font,const int size,const unsigned int bgcolour,const unsigned int fgcolour,const char *text);
+
+void Draw_EORRectangle(const int x,const int y,const int width,const int height,const int linethickness,const unsigned int colour);
+
+void Draw_EORRectangleFilled(const int x,const int y,const int width,const int height,const unsigned int colour);
 
 #endif
