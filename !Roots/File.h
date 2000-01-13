903079b414bffbd7083b27cae57153c94d4a3636
Alex Waugh <alex@alexwaugh.com>
2000-01-13 23:30:53 +0000

    Added File_GetDate

--- !Roots/File.h
+++ !Roots/File.h
@@ -11,6 +11,12 @@ void File_NewFile(void);
 
 void File_Modified(void);
 
+Desk_bool File_GetModified(void);
+
+char *File_GetDate(void);
+
+int File_GetSize(void);
+
 char *File_GetFilename(void);
 
 void File_Result(Desk_save_result result,void *ref);
