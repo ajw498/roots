c01dc59365095f563bf6084d3abc5832f593e280
Alex Waugh <alex@alexwaugh.com>
2000-01-13 18:08:04 +0000

    Added GetFilename and Modified

--- !Roots/File.h
+++ !Roots/File.h
@@ -1,6 +1,18 @@
 #ifndef FILE_H
 #define FILE_H
 
+#ifndef __Desk_Save_h
+#include "Desk.Save.h"
+#endif
+
 Desk_bool File_SaveFile(char *filename,void *ref);
+
+void File_NewFile(void);
+
+void File_Modified(void);
+
+char *File_GetFilename(void);
+
+void File_Result(Desk_save_result result,void *ref);
 
 #endif
