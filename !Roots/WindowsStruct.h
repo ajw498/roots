d5ed64d89f9190255e59c74ae5de3d499bf55239
Alex Waugh <alex@alexwaugh.com>
2000-01-14 13:01:08 +0000

    Changed Graphics_ to Windows_

--- !Roots/WindowsStruct.h
+++ !Roots/WindowsStruct.h
@@ -11,6 +11,9 @@
 
 #define INFINITY 999999
 
+#define EORCOLOUR 0xFFFFFF00
+#define EORCOLOURRED 0xFFFF0000
+
 typedef enum wintype {
 	wintype_NORMAL,
 	wintype_UNLINKED,
@@ -22,21 +25,19 @@ typedef enum wintype {
 typedef void (*plotfn)(const int minx,const int miny,const int maxx,const int maxy,const int linethickness,const unsigned int colour);
 typedef void (*plottextfn)(const int x,const int y,const int handle,const char *font,const int size,const unsigned int bgcolour,const unsigned int fgcolour,const char *text);
 
-void Graphics_Init(void);
+void Windows_Init(void);
 
-void Graphics_Edit(void);
+void Windows_Edit(void);
 
-void Graphics_ChangedLayout(void);
+void Windows_ChangedLayout(void);
 
-void Graphics_OpenWindow(wintype type,elementptr person,int generations);
+void Windows_OpenWindow(wintype type,elementptr person,int generations);
 
-void Graphics_Relayout(void);
+void Windows_Relayout(void);
 
-void Graphics_Redraw(layout *layout,int originx,int originy,Desk_wimp_box *cliprect,Desk_bool plotselection,plotfn plotline,plotfn plotrect,plotfn plotrectfilled,plottextfn plottext);
+int Windows_GetSize(void);
 
-int Graphics_GetSize(void);
-
-void Graphics_Save(FILE *file);
+void Windows_Save(FILE *file);
 
 
 #endif
