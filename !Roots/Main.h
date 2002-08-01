7aee0cc285bdc5fe21aca653f2d58fc35fc3b4f8
Alex Waugh <alex@alexwaugh.com>
2002-08-01 15:55:26 +0000

    Merged bugfix from 1.14 branch Updated version number to 1.15

--- !Roots/Main.h
+++ !Roots/Main.h
@@ -6,8 +6,8 @@
 
 #define ROOTSDIR "<"DIRPREFIX"$Dir>"
 
-#define ROOTS_VERSION_NUMBER 1.14
-#define ROOTS_VERSION "1.14 (20-Oct-01)"
+#define ROOTS_VERSION_NUMBER 1.15
+#define ROOTS_VERSION "1.15 (1-Aug-02)"
 #define ROOTS_FILETYPE 0x197
 
 #endif
