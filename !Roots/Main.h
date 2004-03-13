6ab437506289f7f96aad37959c05d2e805d2db88
Alex Waugh <alex@alexwaugh.com>
2004-03-13 21:45:30 +0000

    Update to 1.17

--- !Roots/Main.h
+++ !Roots/Main.h
@@ -7,7 +7,7 @@
 #define ROOTSDIR "<"DIRPREFIX"$Dir>"
 
 #define ROOTS_VERSION_NUMBER 1.16
-#define ROOTS_VERSION "1.16 (7-Dec-02)"
+#define ROOTS_VERSION "1.17 (13-Mar-04)"
 #define ROOTS_FILETYPE 0x197
 
 #endif
